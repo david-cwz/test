285d27a65096cb359406ac19cb95070b09e3ad8e
david <david_cwz@qq.com>
2022-07-28 14:57:38 +0800

    2022-7-28

--- src/com/david/StringTrans.java
+++ src/com/david/StringTrans.java
@@ -8,6 +8,7 @@ import java.util.*;
  * @date 2019-10-31 09:46
  */
 public class StringTrans {
+    private  static final long serialVersionUID=1;
     public static void main(String[] args) throws IOException {
         String path = "D:\\java_test";
         String fileName_input = "input.txt";
@@ -19,29 +20,16 @@ public class StringTrans {
             return;
         }
         if (!file_input.exists()) {
-            // 创建文件夹
-            //myFile.mkdir();
-            // myFile.mkdirs();
-
-            //创建文件
             file_output.createNewFile();
-
-            // 删除文件夹
-            //myFile.delete();
         }
 
-        // 第三种：以行为单位读取文件，常用于读面向行的格式化文件
         BufferedReader reader = new BufferedReader(new FileReader(file_input));
         String tempString = null;
-        int line = 1;
         Set<String> set = new HashSet<>();
 
-// 一次读入一行，直到读入null为文件结束
         while ((tempString = reader.readLine()) != null) {
             set.add(tempString.trim());
-            // 显示行号
-//            System.out.println("line " + line + ": " + tempString);
-//            line++;
+
         }
         reader.close();
         List<String> list = new ArrayList<>();
@@ -49,11 +37,10 @@ public class StringTrans {
         while (it.hasNext()) {
             list.add(it.next());
         }
-        int n = (int)Math.ceil(list.size() / 1000.0); //计数，每999个输出一个括号,第n段
+        int n = (int)Math.ceil(list.size() / 1000.0);
         System.out.println(n);
         System.out.println(list.size());
 
-        // 第二种：FileWriter(参数true为追加内容，若无则是覆盖内容)
         FileWriter fw = new FileWriter(file_output,true);
         for (int j = 1; j <= n; j++) {
             if (j < n) {
@@ -70,11 +57,6 @@ public class StringTrans {
                 fw.write("'" + list.get(list.size() - 1) + "')\n");
             }
         }
-//        fw.write("(");
-//        for (int i = 0; i < list.size() - 1; i++) {
-//            fw.write("'" + list.get(i) + "',");
-//        }
-//        fw.write("'" + list.get(list.size() - 1) + "')\n");
         fw.close();
     }
 }
--- src/com/david/test.java
+++ src/com/david/test.java
@@ -8,14 +8,33 @@ import java.util.Set;
  * @author chenwz38
  * @date 2019-12-24 09:43
  */
-public class test {
+public class test implements Runnable{
     static boolean flag = true;
     public static void main(String[] args) {
+//        test1();
+        test2();
+    }
 
+    private static void test1() {
         Scanner input = new Scanner(System.in);
         System.out.println(input.next());
     }
 
+    private static void test2() {
+        byte a = 'c';
+//        System.out.println(Integer.toBinaryString(a));
+        String x = Integer.toHexString(a & 0xff) ;
+        byte[] bytes = x.getBytes();
+        for (byte b:bytes) {
+            System.out.println(b + ": " + Integer.toBinaryString(b));
+        }
+//        System.out.println(x);
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
 
 
